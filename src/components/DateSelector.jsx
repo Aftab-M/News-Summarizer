84019ada0c07ddd7e6c379b56b8f0268ca80ab55
Aftab Mulani <93897204+Aftab-M@users.noreply.github.com>
2024-08-07 13:38:11 +0530

    Update DateSelector.jsx

--- src/components/DateSelector.jsx
+++ src/components/DateSelector.jsx
@@ -3,24 +3,17 @@ import React, { useEffect, useState } from 'react';
 const DateSelector = ({ setDate }) => {
 
     const [selectedDate, setSelectedDate] = useState('');
-    // const todayDate = new Date().toISOString().split('T')[0];
-    // const todayDate = "2024-05-06";
     const maxPrevDate = "2024-05-05";
     const todayDate = new Date().toISOString().split('T')[0];
     const todayTime = new Date().toISOString().split('T')[1];
 
     useEffect(() => {
-      // Get today's date
       const today = new Date();
-      // Format the date as "YYYY-MM-DD"
       const formattedDate = today.toISOString().split('T')[0];
-      // console.log('Got the date : ',selectedDate)
       setSelectedDate(formattedDate);
     }, []);
 
     const handleDateChange = (e) => {
-      // console.log('e.target.value : ')
-      // console.log(e.target.value)
       setSelectedDate(e.target.value);
       console.log(todayTime)
       setDate(e.target.value)
